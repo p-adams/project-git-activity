2e4ca05011855be3bb1d6f4ba89809ddc7c90bd5
Paul Adams <ghusiyy@gmail.com>
2022-08-14 00:45:05 -0700

    render tiles

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,29 @@
 import "./style.css";
 
+function Tiles(el: HTMLElement) {
+  function render() {
+    const tileMatrix = new Array(7).fill(0).map(() => new Array(52).fill(0));
+    for (const col of tileMatrix) {
+      for (const _ of col) {
+        const tile = document.createElement("div");
+        tile.setAttribute("class", "tile");
+        el?.appendChild(tile);
+      }
+    }
+  }
+  return { render };
+}
+
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div>
     <article>
       <section>
         <h3>Git Activity Widget</h3>
-        <div id="activity-widget"></div>
+        <div id="activity-widget">
+        </div>
       </section>
     </article>
   </div>
 `;
 
-// setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+Tiles(document.querySelector<HTMLDivElement>("#activity-widget")!).render();
