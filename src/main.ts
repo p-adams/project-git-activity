0d245211ce9ab6d0f7e905b2e4db4b9673f297c3
Paul Adams <ghusiyy@gmail.com>
2022-08-14 00:07:27 -0700

    set width

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,13 @@
 import "./style.css";
-import typescriptLogo from "./typescript.svg";
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-
-    <div id="activity-widget">
-      
-    </div>
-
-
+    <article>
+      <section>
+        <h3>Git Activity Widget</h3>
+        <div id="activity-widget"></div>
+      </section>
+    </article>
   </div>
 `;
 
