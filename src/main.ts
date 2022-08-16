d6f0318edac3ea7aaa3fb3646b141ef591d12b76
Paul Adams <ghusiyy@gmail.com>
2022-08-15 23:50:34 -0700

    display activity levels

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,27 @@
 import "./style.css";
 
+export const levels: { [key: number]: any } = {
+  0: "#ebedf0",
+  1: "#9be9a8",
+  2: "#40c463",
+  3: "#30a14e",
+  4: "#216e39",
+};
+
 function Tiles(el: HTMLElement) {
   function render() {
-    const tileMatrix = new Array(7).fill(0).map(() => new Array(52).fill(0));
-    for (const col of tileMatrix) {
-      for (const _ of col) {
+    const tileMatrix = new Array(7)
+      .fill(0)
+      .map((_) =>
+        new Array(52).fill(0).map((_) => Math.floor(Math.random() * 5))
+      );
+    for (const row of tileMatrix) {
+      for (const value of row) {
         const tileWrapper = document.createElement("div");
         tileWrapper.setAttribute("class", "tile-wrapper");
         const tile = document.createElement("div");
         tile.setAttribute("class", "tile");
+        tile.style.backgroundColor = levels[value];
         tileWrapper.appendChild(tile);
         el?.appendChild(tileWrapper);
       }
